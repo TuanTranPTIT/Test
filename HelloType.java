02f6aabffd09c15d7c16480923fc951511b9382d
TuanTranPTIT <tranthanhtuan01092002@gmail.com>
2024-07-16 11:21:26 +0700

    abc

--- HelloType.java
+++ HelloType.java
@@ -1,7 +1,7 @@
 public class HelloType {
 //gdggaghghashahchgrrrr
     public static void main(){
-        sysout print hello
+        sysout print hello ddd
     }
 //gdggaghghashahchg
     //Đến giờ đi ngủ rồi nhes
